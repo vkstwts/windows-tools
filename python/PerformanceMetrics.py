bfb0086f9e1b1914235b4f66062b5a88cc175ec4
prathap <prathap@9a0cd568-273a-0410-ad19-e1ee4db26171>
2008-05-11 05:02:56 +0000

    add cpu utilization details using sar command.

--- python/PerformanceMetrics.py
+++ python/PerformanceMetrics.py
@@ -4,20 +4,21 @@ import os
 import subprocess as G
 
 dirs = ['/opt/ptc','/vault']
+days = 7 
 
 def sendmail(mesg):
     SENDMAIL = "/usr/sbin/sendmail" # sendmail location
     p = os.popen("%s -t" % SENDMAIL, "w")
-    p.write("To: prathapy@google.com\n")
-    p.write("From: prathapy@google.com\n")
-    p.write("Subject: Frostbite performance details\n")
+    p.write("To: ysprathap@gmail.com\n")
+    p.write("From: windchill@google.com\n")
+    p.write("Subject: Frostbite system monitoring information\n")
     p.write("\n") # blank line separating headers from body
     p.write(mesg)
     sts = p.close()
     if sts != None:
         print "Sendmail exit status", sts
 
-
+#Find Disk Usage details.
 p = G.Popen('df -h', shell=True, stdout=G.PIPE)
 lines = p.stdout.readlines()
 mesg = 'Disk space Utilization details '
@@ -26,7 +27,36 @@ for line in lines:
     current = line.split()
     for dir in dirs:
         if current and len(current)>5 and dir == current[5]:
-            mesg = mesg + '\nPath : '+ current[5]+ '\tUsed space :'+ current[2]+ '\tAvailable  :'+ current[3]
+            mesg = mesg + '\n\nPath : '+ current[5]+ '\nUsed space :'+ current[2]+ '\nAvailable  :'+ current[3]
             break
-sendmail(mesg)
 
+#Find Performance details using sar
+p = G.Popen('ls /var/adm/sa/sa[0-9]* | tail -'+str(days)+' | xargs', shell=True, stdout=G.PIPE) 
+lines = p.stdout.readlines()
+mesg = mesg + '\n\n\nCPU Utilization details\n'
+for line in lines:
+    print line
+    sarFiles = line.split()
+    usr = 0
+    sys = 0
+    wio = 0
+    idle = 0
+    for sarFile in sarFiles: 
+    	cmd = 'sar -f '+sarFile+' | grep Average'
+    	#print cmd
+    	g=G.Popen(cmd, shell=True, stdout=G.PIPE) 
+    	avg= g.stdout.readline()
+    	#print avg
+        util = avg.split()
+        usr = usr + int(util[1])
+        sys = sys + int(util[2])
+        wio = wio + int(util[3])
+        idle = idle + int(util[4])
+
+mesg = mesg + '\nAverage user utilization : '+str(usr/days)
+mesg = mesg + '\nAverage system utilization : '+str(sys/days)
+mesg = mesg + '\nAverage wio utilization : '+str(wio/days)
+mesg = mesg + '\nAverage idle  : '+str(idle/days)
+
+print mesg
+sendmail(mesg)
