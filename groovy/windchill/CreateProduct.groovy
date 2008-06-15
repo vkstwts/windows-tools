00b0648260afc0f72145ff316e4b97b021947bfe
prathap <prathap@9a0cd568-273a-0410-ad19-e1ee4db26171>
2008-06-15 08:26:51 +0000

    added more code. still not working. giving null pointer exception

--- groovy/windchill/CreateProduct.groovy
+++ groovy/windchill/CreateProduct.groovy
@@ -6,30 +6,64 @@ import wt.org.WTPrincipal;
 import wt.org.WTOrganization;
 import wt.session.SessionHelper;
 import wt.query.SearchCondition;
+import wt.inf.container.WTContainerHelper;
+import wt.inf.container.WTContainerRef;
+import wt.inf.template.DefaultWTContainerTemplate;
+import wt.inf.container.OrgContainer;
 
 PDMLinkProduct product = PDMLinkProduct.newPDMLinkProduct();
 product.setName("Test Product");
 //product.setNumber("12345");
-WTPrincipal admin = SessionHelper.manager.getPrincipal();
+
+className = DefaultWTContainerTemplate.class;
+DefaultWTContainerTemplate productTemplate;
+queryspec = new QuerySpec(className);
+//queryspec.appendWhere(new SearchCondition(className, "name", "=", "General Product"), index);
+queryresult = PersistenceHelper.manager.find(queryspec);
+while (queryresult.hasMoreElements()) {
+    productTemplate = (DefaultWTContainerTemplate) queryresult.nextElement();
+    println("DefaultWTContainerTemplate  :"+ productTemplate.getName());
+    if(productTemplate.getName().equals("General Product")) {
+        product.setContainerTemplate(productTemplate);
+        break;
+    }
+}  
+
+WTPrincipal admin = SessionHelper.manager.getAdministrator();
+println("Current user  :"+ admin.getFullName());
 product.setOwner(admin);
-Class className = wt.org.WTOrganization.class;
+product.setCreator(admin);
+
+className = wt.inf.container.OrgContainer.class;
 int[] index = new int[1];
 index[0] = 0;
 QuerySpec queryspec = new QuerySpec(className);
-queryspec.appendWhere(new SearchCondition(className, "name", "=", "BLOOMENERGY"), index);
+queryspec.appendWhere(new SearchCondition(className, "containerInfo.name", "=", "BLOOMENERGY"), index);
 QueryResult queryresult = PersistenceHelper.manager.find(queryspec);
+// WTOrganization bloom ;
+OrgContainer blooml;
 if (queryresult.hasMoreElements()) {
-   WTOrganization bloom = (WTOrganization) queryresult.nextElement();
-   println(bloom.getName());
-   product.setOrganization(bloom);
-   try {
-       bloom = PersistenceHelper.manager.save(bloom);
-       bloom = PersistenceHelper.manager.refresh(bloom);
-       println("After save");
-   } catch (Exception ex) {
-        ex.printStacktrace();
-   }
+   orgContainer = (OrgContainer) queryresult.nextElement();
+   println("orgContainer  :"+orgContainer.getName());
+   //product.setOrganization(bloom);
 }
+//orgContainer  = WTContainerHelper.getContainer(bloom);
+//print(orgContainer);
+orgContainerRef = WTContainerRef.newWTContainerRef(orgContainer);
+product  = WTContainerHelper.setContainer(product,orgContainerRef);
+
+//Create Product
+try {
+    WTContainerHelper.service.create(product);
+   //product = PersistenceHelper.manager.save(product);
+   //product = PersistenceHelper.manager.refresh(product);
+   println("After save");
+} catch (wt.inf.container.WTContainerException ex) {
+    println("Exception raised");
+    ex.getStandardMessage();
+    ex.printStackTrace();
+}
+
 
 className = wt.pdmlink.PDMLinkProduct.class;
 queryspec = new QuerySpec(className);
@@ -40,3 +74,7 @@ while (queryresult.hasMoreElements()) {
    println("Product  :"+ product1.getName());
    //print("Found Product :");
 }
+
+
+
+ 
