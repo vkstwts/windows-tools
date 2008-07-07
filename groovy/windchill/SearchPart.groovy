eb499e7e933d8fec27f653e63d989fe3b437bb27
prathap <prathap@9a0cd568-273a-0410-ad19-e1ee4db26171>
2008-07-07 23:28:01 +0000

    added container and folder information

--- groovy/windchill/SearchPart.groovy
+++ groovy/windchill/SearchPart.groovy
@@ -6,13 +6,13 @@ import wt.fc.QueryResult;
 import wt.fc.PersistenceHelper;
 
 String name="AXLE_LATCH";
-Class className = wt.part.WTPartMaster.class;
-//Class className = wt.part.WTPart.class;
+//Class className = wt.part.WTPartMaster.class;
+Class className = wt.part.WTPart.class;
 int[] index = new int[1];
 index[0] = 0;
 QuerySpec queryspec = new QuerySpec(className);
-queryspec.appendWhere(new SearchCondition(className, "name", "=", name), index);
-//queryspec.appendWhere(new SearchCondition(className, "master->name", "=", name), index);
+//queryspec.appendWhere(new SearchCondition(className, "name", "=", name), index);
+queryspec.appendWhere(new SearchCondition(className, "master>name", "=", name), index);
 QueryResult queryresult = PersistenceHelper.manager.find(queryspec);
 println("Result size :"+ queryresult.size());
 while (queryresult.hasMoreElements()) {
@@ -20,11 +20,15 @@ while (queryresult.hasMoreElements()) {
     if (obj instanceof WTPart) {
         WTPart part = (WTPart) obj;
         String displayIdentifier = part.getDisplayIdentifier();
-     //   if(displayIdentifier.indexOf("GC")==0)  
-           println("Part  :"+displayIdentifier);
+        println("Part  :"+displayIdentifier);
+        println("Container "+ part.getContainerName());
+        println("Folder :"+wt.folder.FolderHelper.service.getFolder(part).getName());  
+        println("Folder Path:"+wt.folder.FolderHelper.getLocation(part));  
+        println("Folder Path:"+wt.folder.FolderHelper.getFolderPath(part));  
     }    else if (obj instanceof WTPartMaster) {
         WTPartMaster partMaster = (WTPartMaster) obj;
         String displayIdentifier = partMaster.getDisplayIdentifier();
         println("PartMaster :"+displayIdentifier);
+        println("Container "+ partMaster.getContainerName());
     }    
 }
