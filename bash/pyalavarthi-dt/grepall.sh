ab47b7034c7da21ba5a3d13b2f10525597bfb15d
prathap <prathap@9a0cd568-273a-0410-ad19-e1ee4db26171>
2008-10-26 03:15:59 +0000

    added staging directories

--- bash/pyalavarthi-dt/grepall.sh
+++ bash/pyalavarthi-dt/grepall.sh
@@ -19,7 +19,28 @@ echo "qa1-service" && cat ~/qa1/Windchill/codebase/service.properties | grep -in
 echo "qa1-ie" && cat ~/qa1/Windchill/codebase/WEB-INF/ie.properties | grep -in $1 
 echo "qa1-db" && cat ~/qa1/Windchill/db/db.properties | grep -in $1
 
-echo "qa2" && cat ~/qa2/Windchill/codebase/wt.properties | grep -in $1
+echo "qa2-wt" && cat ~/qa2/Windchill/codebase/wt.properties | grep -in $1
 echo "qa2-service" && cat ~/qa2/Windchill/codebase/service.properties | grep -in $1 
 echo "qa2-ie" && cat ~/qa2/Windchill/codebase/WEB-INF/ie.properties | grep -in $1 
 echo "qa2-db" && cat ~/qa2/Windchill/db/db.properties | grep -in $1
+
+
+echo "staging-wt" && cat ~/staging/Windchill/codebase/wt.properties | grep -in $1
+echo "staging-service" && cat ~/staging/Windchill/codebase/service.properties | grep -in $1 
+echo "staging-ie" && cat ~/staging/Windchill/codebase/WEB-INF/ie.properties | grep -in $1 
+echo "staging-db" && cat ~/staging/Windchill/db/db.properties | grep -in $1
+
+echo "slave1-wt" && cat ~/slave1/Windchill/codebase/wt.properties | grep -in $1
+echo "slave1-service" && cat ~/slave1/Windchill/codebase/service.properties | grep -in $1 
+echo "slave1-ie" && cat ~/slave1/Windchill/codebase/WEB-INF/ie.properties | grep -in $1 
+echo "slave1-db" && cat ~/slave1/Windchill/db/db.properties | grep -in $1
+
+echo "slave2-wt" && cat ~/slave2/Windchill/codebase/wt.properties | grep -in $1
+echo "slave2-service" && cat ~/slave2/Windchill/codebase/service.properties | grep -in $1 
+echo "slave2-ie" && cat ~/slave2/Windchill/codebase/WEB-INF/ie.properties | grep -in $1 
+echo "slave2-db" && cat ~/slave2/Windchill/db/db.properties | grep -in $1
+
+echo "slave3-wt" && cat ~/slave3/Windchill/codebase/wt.properties | grep -in $1
+echo "slave3-service" && cat ~/slave3/Windchill/codebase/service.properties | grep -in $1 
+echo "slave3-ie" && cat ~/slave3/Windchill/codebase/WEB-INF/ie.properties | grep -in $1 
+echo "slave3-db" && cat ~/slave3/Windchill/db/db.properties | grep -in $1
