4106388050b839f63afd86b586068efa074a7b2f
prathap <prathap@9a0cd568-273a-0410-ad19-e1ee4db26171>
2008-11-08 08:16:28 +0000

    applied command pattern

--- python/WindchillUtility/windutil.py
+++ python/WindchillUtility/windutil.py
@@ -3,100 +3,117 @@ import time
 import sys
 import subprocess as G
 
-#Define Global Variables
-cmd='psexec'
-ddrive='D:'
-edrive='E:'
-win9=edrive+'\\ptc\\Windchill_9.0'
-wind=win9+'\\Windchill'
-unix=win9+'\\UnxUtils\\usr\\local\\wbin\\'
+class Dispatcher:
+    #Define Global Variables
+    cmd='psexec'
+    ddrive='D:'
+    edrive='E:'
+    win9=edrive+'\\ptc\\Windchill_9.0'
+    wind=win9+'\\Windchill'
+    unix=win9+'\\UnxUtils\\usr\\local\\wbin\\'
 
-version=wind+'\\bin\\windchill.exe --java='+win9+'\\Java\\bin\\java.exe version'
-xconf=wind+'\\bin\\xconfmanager.bat -p'
-touch=unix+'touch.exe '+wind+'\\site.xconf'
-property=unix+'cat.exe '+wind+'\\codebase\\wt.properties '+wind+'\\codebase\\service.properties '+wind+'\\codebase\\WEB-INF\\ie.properties '+wind+'\\db\\db.properties | findstr /i'
-winstatus=wind+'\\bin\\windchill.exe --java='+win9+'\\Java\\bin\\java.exe status'
+    version=wind+'\\bin\\windchill.exe --java='+win9+'\\Java\\bin\\java.exe version'
+    xconf=wind+'\\bin\\xconfmanager.bat -p'
+    touch=unix+'touch.exe '+wind+'\\site.xconf'
+    property=unix+'cat.exe '+wind+'\\codebase\\wt.properties '+wind+'\\codebase\\service.properties '+wind+'\\codebase\\WEB-INF\\ie.properties '+wind+'\\db\\db.properties | findstr /i'
+    winstatus=wind+'\\bin\\windchill.exe --java='+win9+'\\Java\\bin\\java.exe status'
 
-statusFile='C:\\Dev\\bin\\windStatus.bat'
-stageserversFile='C:\\Dev\\bin\\stagingServers.txt'
-prodserversFile='C:\\Dev\\bin\\prodServers.txt'
+    statusFile='C:\\Dev\\bin\\windStatus.bat'
+    stageserversFile='C:\\Dev\\bin\\stagingServers.txt'
+    prodserversFile='C:\\Dev\\bin\\prodServers.txt'
 
-servers = ['\\\\hqdvpttmp01','\\\\hqqapttmp01','\\\\hqqapttmp02','\\\\hqdvpttg01']
-stageservers = ['\\\\hqstptas01','\\\\hqstptws01','\\\\hqstptws02','\\\\hqstptws03']
-prodservers = ['\\\\hqnvptas01','\\\\hqnvptws03','\\\\hqnvptws04','\\\\hqnvptws05','\\\\hqnvptws06']
-allservers = servers + stageservers + prodservers
-server = '\\\\hqdvpttmp01'
+    servers = ['\\\\hqdvpttmp01','\\\\hqqapttmp01','\\\\hqqapttmp02','\\\\hqdvpttg01']
+    stageservers = ['\\\\hqstptas01','\\\\hqstptws01','\\\\hqstptws02','\\\\hqstptws03']
+    prodservers = ['\\\\hqnvptas01','\\\\hqnvptws03','\\\\hqnvptws04','\\\\hqnvptws05','\\\\hqnvptws06']
+    allservers = servers + stageservers + prodservers
 
- 
-def propogateXconf(server):
-    touchcmd=cmd+" "+server+" "+touch
-    fullcmd=cmd+" "+server+" "+xconf
-    print "Running on Server :"+server
-    runCommand(touchcmd)
-    runCommand(fullcmd)
+    def propogateXconf(self,server):
+        touchcmd=self.cmd+" "+server+" "+self.touch
+        fullcmd=self.cmd+" "+server+" "+self.xconf
+        self.runCommand(touchcmd)
+        self.runCommand(fullcmd)
 
-def propogateXconf1(servers):
-    for server in servers:
-        propogateXconf(server)
+    def propogateXconf1(self,serversFile):
+        touchcmd=self.cmd+" @"+serversFile+" "+self.touch
+        fullcmd=self.cmd+" @"+serversFile+" "+self.xconf
+        self.runCommand(touchcmd)
+        self.runCommand(fullcmd)
 
-def findVersion(server):
-    versioncmd=cmd+" "+server+" "+version
-    print "Running on Server :"+server
-    runCommand(versioncmd)
+    def findVersion(self,server):
+        versioncmd=self.cmd+" "+server+" "+self.version
+        self.runCommand(versioncmd)
 
-def findVersion1(servers):
-    for server in servers:
-        findVersion(server)
+    def findVersion1(self,serversFile):
+        versioncmd=self.cmd+" @"+serversFile+" "+self.version
+        self.runCommand(versioncmd)
 
-def findProperty(server,searchString):
-    propertycmd=cmd+" "+server+" "+property+" "+searchString
-    print "Running on Server :"+server
-    runCommand(propertycmd)
+    def findProperty(self,server):
+        searchString = raw_input("Enter Search String :")
+        propertycmd=self.cmd+" "+server+" "+self.property+" "+searchString
+        self.runCommand(propertycmd)
 
-def findProperty1(serversFile,searchString):
-    propertycmd=cmd+" @"+serversFile+" "+property+" "+searchString
-    runCommand(propertycmd)
-      
-def findStatus(server):
-    statuscmd=cmd+" "+server+" -c "+statusFile
-    print "statuscmd :"+statuscmd
-    runCommand(statuscmd)
+    def findProperty1(self,serversFile):
+        searchString = raw_input("Enter Search String :")
+        propertycmd=self.cmd+" @"+serversFile+" "+self.property+" "+searchString
+        self.runCommand(propertycmd)
+          
+    def findStatus(self,server):
+        statuscmd=self.cmd+" "+server+" -c "+self.statusFile
+        self.runCommand(statuscmd)
 
-def findStatus1(serversFile):
-    statuscmd=cmd+" @"+serversFile+" -c "+statusFile
-    runCommand(statuscmd)
+    def findStatus1(self,serversFile):
+        statuscmd=self.cmd+" @"+serversFile+" -c "+self.statusFile
+        self.runCommand(statuscmd)
 
-def findWindchillStatus(server):
-    winstatuscmd=cmd+" "+server+" "+winstatus
-    print "winstatuscmd :"+winstatuscmd
-    runCommand(winstatuscmd)
+    def findWindchillStatus(self,server):
+        winstatuscmd=self.cmd+" "+server+" "+self.winstatus
+        self.runCommand(winstatuscmd)
 
-def findWindchillStatus1(serversFile):
-    winstatuscmd=cmd+" @"+serversFile+" "+winstatus
-    runCommand(winstatuscmd)
-
+    def findWindchillStatus1(self,serversFile):
+        winstatuscmd=self.cmd+" @"+serversFile+" "+self.winstatus
+        self.runCommand(winstatuscmd)
+     
+    def updateCommands(self,fromDrive, toDrive):
+        self.version = self.version.replace(fromDrive,toDrive)
+        self.xconf = self.xconf.replace(fromDrive,toDrive)
+        self.touch = self.touch.replace(fromDrive,toDrive)
+        self.property = self.property.replace(fromDrive,toDrive)
+        self.winstatus = self.winstatus.replace(fromDrive,toDrive)
+        
+    def runCommand(self,command):
+        print 'Command :'+ command
+        retcode = G.call(command,shell=True)
+        print "Return code :" + str(retcode)
+    ##    p = G.Popen(command, shell=True, stdout=G.PIPE)
+    ##    lines = p.stdout.readlines()
+    ##    for line in lines:
+    ##        print line
     
-def updateCommands(fromDrive, toDrive):
-    global version
-    global xconf
-    global touch
-    global property
-    version = version.replace(fromDrive,toDrive)
-    xconf = xconf.replace(fromDrive,toDrive)
-    touch = touch.replace(fromDrive,toDrive)
-    property = property.replace(fromDrive,toDrive)
+    def error(self):
+        print 'Error'
 
-def runCommand(command):
-    print 'Command :'+ command
-##    os.system(command)
-##    os.system('dir')
-    retcode = G.call(command,shell=True)
-    print "Return code :" + str(retcode)
-##    p = G.Popen(command, shell=True, stdout=G.PIPE)
-##    lines = p.stdout.readlines()
-##    for line in lines:
-##        print line
-        #current = line.split()
+    def dispatch(self, command):
+        mname = command
+        if hasattr(self, mname):
+            method = getattr(self, mname)
+            method1 = getattr(self,mname+"1")
+            if serverOption==STAGING:
+                method1(self.stageserversFile)
+            elif serverOption==PRODUCTION:
+                method1(self.prodserversFile)
+            elif serverOption==ALL:
+                method1(self.allservers)
+            elif serverOption==DEV:
+                self.updateCommands("E:","D:")
+                server = self.servers[serverOption-1]
+                method(server)
+                self.updateCommands("D:","E:")
+            else:
+                server = self.servers[serverOption-1]
+                method(server)            
+        else:
+            self.error()
+            
 
 DEV = 1
 QA1 = 2
@@ -143,162 +160,18 @@ while True:
         print 'Selected Server :'+str(serverOption)
         printActions()
         action = int(raw_input())
+        d = Dispatcher()
         if action==EXIT_ACTIONS:
             sys.exit()
         elif action==PROPOGATE_XCONF:
-            if serverOption==STAGING:
-                propogateXconf1(stageservers)
-                continue
-            elif serverOption==PRODUCTION:
-                propogateXconf1(prodservers)
-                continue
-            elif serverOption==ALL:
-                propogateXconf1(allservers)
-                continue
-            elif serverOption==DEV:
-                updateCommands(edrive,ddrive)
-                server = servers[serverOption-1]
-                propogateXconf(server)
-                updateCommands(ddrive,edrive)
-                continue
-            server = servers[serverOption-1]
-            propogateXconf(server)
+            d.dispatch('propogateXconf')
         elif action==FIND_VERSION:
-            if serverOption==STAGING:
-                findVersion1(stageservers)
-                continue
-            elif serverOption==PRODUCTION:
-                findVersion1(prodservers)
-                continue
-            elif serverOption==ALL:
-                findVersion1(allservers)
-                continue
-            elif serverOption==DEV:
-                updateCommands("E:","D:")
-                server = servers[serverOption-1]
-                findVersion(server)
-                updateCommands("D:","E:")
-                continue
-            server = servers[serverOption-1]
-            findVersion(server)
+            d.dispatch('findVersion')
         elif action==FIND_PROPERTY:
-            searchString = raw_input("Enter Search String :")
-            if serverOption==STAGING:
-                findProperty1(stageserversFile,searchString)
-                continue
-            elif serverOption==PRODUCTION:
-                findProperty1(prodserversFile,searchString)
-                continue
-            elif serverOption==ALL:
-                findProperty1(allservers,searchString)
-                continue
-            elif serverOption==DEV:
-                updateCommands("E:","D:")
-                server = servers[serverOption-1]
-                findProperty(server,searchString)
-                updateCommands("D:","E:")
-                continue
-            server = servers[serverOption-1]
-            findProperty(server,searchString)
+            d.dispatch('findProperty')
         elif action==FIND_SERVICES_STATUS:
-            if serverOption==STAGING:
-                findStatus1(stageserversFile)
-                continue
-            elif serverOption==PRODUCTION:
-                findStatus1(prodserversFile)
-                continue
-            elif serverOption==ALL:
-                findStatus1(allservers)
-                continue
-            elif serverOption==DEV:
-                updateCommands("E:","D:")
-                server = servers[serverOption-1]
-                findStatus(server)
-                updateCommands("D:","E:")
-                continue
-            server = servers[serverOption-1]
-            findStatus(server)
+            d.dispatch('findStatus')
         elif action==FIND_WINDCHILL_STATUS:
-            if serverOption==STAGING:
-                findWindchillStatus1(stageserversFile)
-                continue
-            elif serverOption==PRODUCTION:
-                findWindchillStatus1(prodserversFile)
-                continue
-            elif serverOption==ALL:
-                findWindchillStatus1(allservers)
-                continue
-            elif serverOption==DEV:
-                updateCommands("E:","D:")
-                server = servers[serverOption-1]
-                findWindchillStatus(server)
-                updateCommands("D:","E:")
-                continue
-            server = servers[serverOption-1]
-            findWindchillStatus(server)
+            d.dispatch('findWindchillStatus')
         elif action==CHANGE_SERVER:
             break    
-
-
-##class Dispatcher:
-##    
-##    def error(self):
-##
-##    def do_propogateXconf(server):
-##        touchcmd=cmd+" "+server+" "+touch
-##        fullcmd=cmd+" "+server+" "+xconf
-##        print "Running on Server :"+server
-##        runCommand(touchcmd)
-##        runCommand(fullcmd)
-##
-##    def do_propogateXconf1(servers):
-##        for server in servers:
-##            propogateXconf(server)
-##
-##    def do_findVersion(server):
-##        versioncmd=cmd+" "+server+" "+version
-##        print "Running on Server :"+server
-##        runCommand(versioncmd)
-##
-##    def do_findVersion1(servers):
-##        for server in servers:
-##            findVersion(server)
-##
-##    def do_findProperty(server,searchString):
-##        propertycmd=cmd+" "+server+" "+property+" "+searchString
-##        print "Running on Server :"+server
-##        runCommand(propertycmd)
-##
-##    def do_findProperty1(servers,searchString):
-##        for server in servers:
-##            findProperty(server,searchString)
-##             
-##    def do_findStatus(server):
-##        statuscmd=cmd+" "+server+" -c "+statusFile
-##        print "Running on Server :"+server
-##        print "statuscmd :"+statuscmd
-##        runCommand(statuscmd)
-##
-##    def do_findStatus1(serversFile):
-##        statuscmd=cmd+" @"+serversFile+" -c "+statusFile
-##        print "Running on Server :"+server
-##        runCommand(statuscmd)
-##
-##    def do_updateCommands(fromDrive, toDrive):
-##        global version
-##        global xconf
-##        global touch
-##        global property
-##        version = version.replace(fromDrive,toDrive)
-##        xconf = xconf.replace(fromDrive,toDrive)
-##        touch = touch.replace(fromDrive,toDrive)
-##        property = property.replace(fromDrive,toDrive)
-##
-##    
-##    def dispatch(self, command):
-##        mname = 'do_' + command
-##        if hasattr(self, mname):
-##            method = getattr(self, mname)
-##            method()
-##        else:
-##            self.error()
