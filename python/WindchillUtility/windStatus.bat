14187f65a6009006b851cd4fd58db94dea6dd82c
prathap <prathap@9a0cd568-273a-0410-ad19-e1ee4db26171>
2008-11-08 06:33:44 +0000

    windchill utility related files.

---
+++ python/WindchillUtility/windStatus.bat
@@ -0,0 +1,10 @@
+@echo off
+rem hostname
+echo Apache 
+sc query  ptcapache | findstr /i "state "
+echo Tomcat
+sc query  ptctomcat | findstr /i "state "
+echo Windchill
+sc query  ptcwindchill | findstr /i "state "
+echo Partslink
+sc query  Partslink | findstr /i "state "
