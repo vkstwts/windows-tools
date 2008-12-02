62e2d9751cd37b306ba724dd7aafcb22a61f7051
prathap <prathap@9a0cd568-273a-0410-ad19-e1ee4db26171>
2008-12-02 22:01:11 +0000

    added tomcat errors.

--- python/WindchillUtility/mailErrors.py
+++ python/WindchillUtility/mailErrors.py
@@ -23,12 +23,18 @@ def sendTextMail(to,subject,text):
     smtp.sendmail(frm, [to], mail.as_string())
     smtp.close()
 
-
 commandsList = [ 'ls.exe -tr \\\\hqnvptas01\\E$\\ptc\\Windchill_9.0\\Windchill\\logs\\MethodServer-* | tail.exe -2  | xargs grep -in -ferrors.txt',
-                'ls.exe -tr \\\\hqnvptws03\\E$\\ptc\\Windchill_9.0\\Windchill\\logs\\MethodServer-* | tail.exe -4  | xargs grep -in -ferrors.txt',
-                'ls.exe -tr \\\\hqnvptws04\\E$\\ptc\\Windchill_9.0\\Windchill\\logs\\MethodServer-* | tail.exe -4  | xargs grep -in -ferrors.txt',
                 'ls.exe -tr \\\\hqnvptws05\\E$\\ptc\\Windchill_9.0\\Windchill\\logs\\MethodServer-* | tail.exe -4  | xargs grep -in -ferrors.txt',
                 'ls.exe -tr \\\\hqnvptws06\\E$\\ptc\\Windchill_9.0\\Windchill\\logs\\MethodServer-* | tail.exe -4  | xargs grep -in -ferrors.txt']
+				# ,
+                # 'ls.exe -tr \\\\hqnvptws03\\E$\\ptc\\Windchill_9.0\\Windchill\\logs\\MethodServer-* | tail.exe -4  | xargs grep -in -ferrors.txt',
+                # 'ls.exe -tr \\\\hqnvptws04\\E$\\ptc\\Windchill_9.0\\Windchill\\logs\\MethodServer-* | tail.exe -4  | xargs grep -in -ferrors.txt']
+tomcatCommandsList = ['grep -n -ferrors.txt  \\\\hqnvptas01\\E$\\ptc\\Windchill_9.0\\Tomcat\\logs\\PTCTomcat-stdout.log',
+					  'grep -n -ferrors.txt  \\\\hqnvptas01\\E$\\ptc\\Windchill_9.0\\Tomcat\\logs\\windchill.log',
+					  'grep -n -ferrors.txt  \\\\hqnvptws05\\E$\\ptc\\Windchill_9.0\\Tomcat\\logs\\PTCTomcat-stdout.log',
+					  'grep -n -ferrors.txt \\\\hqnvptws05\\E$\\ptc\\Windchill_9.0\\Tomcat\\logs\\windchill.log',
+					  'grep -n -ferrors.txt  \\\\hqnvptws06\\E$\\ptc\\Windchill_9.0\\Tomcat\\logs\\PTCTomcat-stdout.log',
+					  'grep -n -ferrors.txt  \\\\hqnvptws06\\E$\\ptc\\Windchill_9.0\\Tomcat\\logs\\windchill.log']
 
 mesg = MIMEMultipart()
 today =datetime.datetime.today()
@@ -41,13 +47,16 @@ startMin = int(currentMin)-14
 #print startMin
 
 filename="\\\\hqdvpttmp01\\ExceptionsInProductionlogs\\WindchillMethodServerErrors_"+todayStr.replace("/","_").replace(" ","__").replace(":","_")+currentMin+".txt"
+print filename
 f = open(filename,'w')
 foundErrors=0
 try:
-	for command in commandsList:
+	for command in commandsList+tomcatCommandsList:
 		p = G.Popen(command, shell=True, stdout=G.PIPE)
 		lines = p.stdout.readlines()
+		print command
 		for line in lines:
+			startMin = int(currentMin)-14 
 			while startMin <=int(currentMin):
 				if startMin < 10:
 					searchTimeStr = todayStr+'0'+str(startMin)
@@ -55,12 +64,14 @@ try:
 					searchTimeStr =  todayStr+str(startMin)
 				startMin +=1
 				#print searchTimeStr
-				if(line.find(searchTimeStr)>0):
-					foundErrors=1
-					f.write(line)
+				#searchTimeStr = '2008-12-02 09:'
+			if(line.find(searchTimeStr)>0):
+				print line
+				foundErrors=1
+				f.write(line)
 finally:
     f.close()
-
+print todayStr
 mailSubject =  mailSubject+searchTimeStr
 preambleText='Windchill MethodServer logs are searched for the following error messages and found atleast one of them in the last 15 min.\n\n'
 
@@ -76,7 +87,7 @@ preambleText=preambleText+"\n You can find the attached log messages in the foll
 mesg.preamble=preambleText
 mesg.epilogue='\nEnd of log Messages\n'
 
-#foundErrors=1
+foundErrors=1
 if foundErrors==1:
 	f = open(filename,'r')
 	mesg.attach(MIMEText(f.read()))
@@ -84,5 +95,5 @@ if foundErrors==1:
 	for toaddr in mailTo:
 		sendTextMail(toaddr,mailSubject,mesg.as_string())
 else:
-	# print filename
+	#print filename 
 	os.remove(filename)
