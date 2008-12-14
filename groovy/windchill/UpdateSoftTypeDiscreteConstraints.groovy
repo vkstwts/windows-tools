714cbb12ac680ee11015a6338a13dd07006c5856
prathap <prathap@9a0cd568-273a-0410-ad19-e1ee4db26171>
2008-12-14 12:48:34 +0000

    able to update the discrete contraint on IBA

--- groovy/windchill/UpdateSoftTypeDiscreteConstraints.groovy
+++ groovy/windchill/UpdateSoftTypeDiscreteConstraints.groovy
@@ -7,10 +7,25 @@ import wt.fc.PersistenceHelper;
 import com.ptc.core.meta.server.TypeIdentifierUtility;
 //import wt.session.SessionHelper;
 import wt.type.*;
+import wt.util.*;
+import com.ptc.core.meta.container.common.*;
 import com.ptc.core.meta.common.*;
 import com.ptc.core.meta.common.impl.*;
 import com.ptc.core.meta.type.admin.common.impl.*;
 import com.ptc.core.meta.type.mgmt.common.*;
+import com.ptc.core.meta.type.mgmt.server.*;
+import com.ptc.core.meta.type.mgmt.server.impl.*;
+import com.ptc.core.meta.type.mgmt.server.impl.service.*;
+
+import com.ptc.core.meta.type.mgmt.common.*;
+import com.ptc.core.meta.type.mgmt.server.*;
+import com.ptc.core.meta.type.mgmt.server.impl.*;
+import com.ptc.core.meta.type.mgmt.server.impl.service.*;
+import wt.method.RemoteMethodServer;
+
+RemoteMethodServer methodServer = RemoteMethodServer.getDefault();
+methodServer.setUserName("wcadmin");
+methodServer.setPassword("wcadmin");
 
 //println SessionHelper.getPrincipal().getName();
 //SessionHelper.manager.setAdministrator();
@@ -23,11 +38,13 @@ String persistedType = TypedUtilityServiceHelper.service.getExternalTypeIdentifi
 System.out.println(persistedType);        
 //TypeIdentifier typeidentifier = (new TypeIdentifierUtility()).getTypeIdentifierFromPersistedType(persistedType);
 //System.out.println(typeidentifier);
- //TypedUtilityServiceHelper.service.initTypeDefinitions();
+//TypedUtilityServiceHelper.service.initTypeDefinitions();
 
-TypeDefinitionReference typeDefRef = TypedUtilityServiceHelper.service.getTypeDefinitionReference("wt.part.WTPart|com.nvidia.Part");
-System.out.println(typeDefRef.toString());        
-System.out.println(typeDefRef.getKey().toString()); 
+TypeDefinitionReference typeDefRef = TypedUtilityServiceHelper.service.getTypeDefinitionReference("wt.part.WTPart");
+System.out.println("*********"+typeDefRef.toString());        
+System.out.println("*********"+typeDefRef.getKey().toString()); 
+
+
 TypeDefinitionNodeView[] nodes = TypeAdminHelper.service.getTypeNodeRoots();
 for(node in nodes){
     String nodeName = node.getName();
@@ -37,24 +54,44 @@ for(node in nodes){
         for(childNode in childNodes){
             String childNodeName = childNode.getName();
             System.out.println("--->"+childNodeName);
-             if(childNodeName.equals("com.nvidia.Part")){
-                  System.out.println("checking out "+childNodeName);
-                  //childNode = TypeAdminHelper.service.checkoutTypeNode((TypeDefinitionNodeView)childNode,java.util.Locale.US);
-                   //childNode = TypeAdminHelper.service.undoCheckoutTypeNode((TypeDefinitionNodeView)childNode,true,java.util.Locale.US);
-                   defaultView = TypeAdminHelper.service.getTypeDefDefaultView(childNode,java.util.Locale.US);
-                   typeIdentifiers =  defaultView.getConstraintContainer().getConstraintTypeIdentifiers();
-                   for (identifier in typeIdentifiers) {
-                           //if (identifier.getContext() instanceof DiscreteSet )
-                               println(identifier);
-                    }
-                   
-             }
+            if(childNodeName.equals("com.nvidia.Part")){
+                System.out.println("checking out "+childNodeName);
+                      childNode = TypeAdminHelper.service.checkoutTypeNode((TypeDefinitionNodeView)childNode,java.util.Locale.US);
+                    
+                defaultView = TypeAdminHelper.service.getTypeDefDefaultView(childNode,java.util.Locale.US);
+                identifiers =  defaultView.getConstraintContainer().getConstraintIdentifiers(1);
+                for (identifier in identifiers) {
+                     if (identifier.toString().indexOf("Part_ProductLine")>0
+                       && identifier.getEnforcementRuleClassname().equalsIgnoreCase("com.ptc.core.meta.container.common.impl.DiscreteSetConstraint") ){
+                        println("----------->"+identifier);
+                    ConstraintData constraintData = defaultView.getConstraintContainer().get(identifier);
+                    println constraintData.getBindingRuleData();
+                    //println constraintData.getEnforcementRuleData();
+                    DiscreteSet enforcementRuleData = constraintData.getEnforcementRuleData();
+                    println enforcementRuleData;
+                    DiscreteSet newData = new DiscreteSet("Test2");
+                    constraintData.setEnforcementRuleData(enforcementRuleData.getUnion(newData))
+                     println constraintData.getEnforcementRuleData();
+                    defaultView.getConstraintContainer().put(identifier,constraintData);
+                    try {
+                     defaultView = TypeAdminHelper.service.updateTypeDefDefaultView(defaultView,java.util.Locale.US);
+                     println defaultView;
+                    childNode = TypeAdminHelper.service.checkinTypeNode(null,(TypeDefinitionNodeView)childNode,java.util.Locale.US);
+                    System.out.println("checking in "+childNodeName);
+                 
+                    } catch (WTException exception) {
+                    System.out.println("undoing checking out "+childNodeName);
+                 
+                   childNode = TypeAdminHelper.service.undoCheckoutTypeNode((TypeDefinitionNodeView)childNode,true,java.util.Locale.US);
+                    println exception;
+                   // childNode = TypeAdminHelper.service.checkinTypeNode(null,(TypeDefinitionNodeView)childNode,java.util.Locale.US);
+                        }
+                   }
+                     
+                }
+                break;           
+            }
+            
         }
     }
- }
-//WTPart part = WTPart.newWTPart();
-//part.setName("test Part 101");
-//part.setNumber("test Part 101");
-//part.setTypeDefinitionReference(typeDefRef);
-//part = (WTPart)PersistenceHelper.manager.save(part);
-//System.out.println(part.getDisplayIdentifier());
+}
